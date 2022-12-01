023c6f9d712825f4a33bbf0c407fe0d4a7705fb1
Neil Studd <github@neilstudd.com>
2022-12-01 16:53:40 +0000

    Prevent Pylance from complaining about imports

--- 2022/day01/day01a.py
+++ 2022/day01/day01a.py
@@ -1,6 +1,6 @@
 import sys, os
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'common'))
-from common import *
+from common import open_file
 
 file_content = open_file("input.txt")
 
