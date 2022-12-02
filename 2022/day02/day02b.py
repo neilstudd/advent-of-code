59f2bb34dac52903a06978917051876378b92df9
Neil Studd <github@neilstudd.com>
2022-12-02 13:25:29 +0000

    See, I told you I mixed them up

--- 2022/day02/day02b.py
+++ 2022/day02/day02b.py
@@ -7,16 +7,16 @@ file_content = open_file("input.txt")
 ROCK = 1
 PAPER = 2
 SCISSORS = 3
-WIN = "X"
+LOSS = "X"
 DRAW = "Y"
-LOSS = "Z"
+WIN = "Z"
 
-OUTCOMES = {WIN: 0, DRAW: 3, LOSS: 6}
+OUTCOMES = {LOSS: 0, DRAW: 3, WIN: 6}
 
 STRATEGY_GUIDE = {
-    "A": {WIN: SCISSORS, DRAW: ROCK, LOSS: PAPER},
-    "B": {WIN: ROCK, DRAW: PAPER, LOSS: SCISSORS},
-    "C": {WIN: PAPER, DRAW: SCISSORS, LOSS: ROCK}
+    "A": {LOSS: SCISSORS, DRAW: ROCK, WIN: PAPER},
+    "B": {LOSS: ROCK, DRAW: PAPER, WIN: SCISSORS},
+    "C": {LOSS: PAPER, DRAW: SCISSORS, WIN: ROCK}
 }
 
 score = 0
