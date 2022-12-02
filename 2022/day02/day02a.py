a188d54e73d05479f3c1a9679fc8a1f92733d3d3
Neil Studd <github@neilstudd.com>
2022-12-02 05:56:25 +0000

    AoC 2022 Day 2

--- 2022/day02/day02a.py
+++ 2022/day02/day02a.py
@@ -4,6 +4,17 @@ from common import open_file
 
 file_content = open_file("input.txt")
 
+YOUR_THROW = {"X": 1, "Y": 2, "Z": 3}
+TRANSLATE_YOUR_THROW_TO_THEIRS = {"X": "A", "Y": "B", "Z": "C"}
+score = 0
+
 for line in file_content:
-    # do stuff
-    print(line.strip())
+    thisRound = line.strip().split(" ")
+    oppGuess = thisRound[0]
+    yourGuess = thisRound[1]
+
+    score += YOUR_THROW[yourGuess]
+    score += 3 if oppGuess == TRANSLATE_YOUR_THROW_TO_THEIRS[yourGuess] else 0
+    score += 6 if (oppGuess == "A" and yourGuess == "Y") or (oppGuess == "B" and yourGuess == "Z") or (oppGuess == "C" and yourGuess == "X") else 0  
+
+print(score) # 10624
--- 2022/day02/day02b.py
+++ 2022/day02/day02b.py
@@ -4,6 +4,24 @@ from common import open_file
 
 file_content = open_file("input.txt")
 
+ROCK = 1, 
+PAPER = 2
+SCISSORS = 3
+
+OUTCOMES = {"X": 0, "Y": 3, "Z": 6}
+
+STRATEGY_GUIDE = {
+    "A": {"X": SCISSORS, "Y": ROCK, "Z": PAPER},
+    "B": {"X": ROCK, "Y": PAPER, "Z": SCISSORS},
+    "C": {"X": PAPER, "Y": SCISSORS, "Z": ROCK}
+}
+
+score = 0
+
 for line in file_content:
-    # do stuff
-    print(line.strip())
+    thisRound = line.strip().split(" ")
+    oppGuess = thisRound[0]
+    yourGuess = thisRound[1]
+    score += OUTCOMES[yourGuess] + STRATEGY_GUIDE[oppGuess][yourGuess]
+
+print(score) # 14060
