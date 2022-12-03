1b16cc4cf5ca092558b13ff2285deb0cad704e57
Neil Studd <github@neilstudd.com>
2022-12-03 07:59:30 +0000

    AoC 2022 day 3

--- 2022/day03/day03a.py
+++ 2022/day03/day03a.py
@@ -3,7 +3,24 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'common'))
 from common import open_file
 
 file_content = open_file("input.txt")
+total_priorities = 0
+commonItems = []
+
+# Build alphabet dictionary (a-z = 1-26, A-Z = 27-52)
+alphabet = {}
+for i in range(1, 27):
+    alphabet[chr(96+i)] = i
+    alphabet[chr(64+i)] = i+26
 
 for line in file_content:
-    # do stuff
-    print(line.strip())
+    thisRucksack = line.strip()
+    compartment1, compartment2 = thisRucksack[:len(line)//2], thisRucksack[len(line)//2:]
+    for item in compartment1:
+        if item in compartment2:
+            commonItems.append(item)
+            break
+
+for item in commonItems:
+    total_priorities += alphabet[item]
+
+print(total_priorities) # 8085
