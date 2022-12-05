cecbc8f1f783be109d76e0e65a9d85496f74b4dc
Neil Studd <github@neilstudd.com>
2022-12-05 06:30:03 +0000

    AoC day 5 pain

--- 2022/day05/day05b.py
+++ 2022/day05/day05b.py
@@ -1,9 +1,44 @@
-import sys, os
+import sys, os, re
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'common'))
 from common import open_file
 
 file_content = open_file("input.txt")
 
+# Ugh hardcoded
+stacks = 9
+stack_array = [[],[],[],[],[],[],[],[],[]]
+starting_depth = 8
+
+line_count = 0
 for line in file_content:
-    # do stuff
-    print(line.strip())
+    line_count += 1
+    if line_count <= starting_depth:
+        line.strip()
+        current_index = 0
+        for i in range(1, stacks*4, 4):
+            thisItem = line[i:i+1]
+            stack_array[current_index].insert(0, thisItem) if thisItem != " " else None
+            current_index += 1
+    else:
+        if line[0:4] == "move":
+            numbers = re.findall(r'\d+', line)
+            number_to_move = int(numbers[0])
+            move_from = int(numbers[1])
+            move_to = int(numbers[2])
+            items_to_move = []
+            for x in range(number_to_move):
+                items_to_move.append(stack_array[move_from-1].pop())
+            items_to_move.reverse()
+            for item in items_to_move:
+                stack_array[move_to-1].append(item)
+
+topItems = ""
+for i in range(stacks):
+    topItems += stack_array[i][-1]
+print(topItems) # LCTQFBVZV
+
+
+
+
+
+
