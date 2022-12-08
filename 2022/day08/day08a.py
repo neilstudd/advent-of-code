bef38949d67fc6ab48420b28e910542cd66fb34b
Neil Studd <github@neilstudd.com>
2022-12-08 08:15:13 +0000

    AoC 2022 day 8 refactored

--- 2022/day08/day08a.py
+++ 2022/day08/day08a.py
@@ -2,55 +2,18 @@ import sys, os
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'common'))
 from common import open_file
 
-file_content = open_file("input.txt")
-
-tree_data = []
-
-for line in file_content:
-    tree_data.append(list(line.strip()))
-
-def tree_on_edge(row, col, tree_data):
-    if row == 0 or row == len(tree_data)-1:
-        return True
-    if col == 0 or col == len(tree_data[0])-1:
-        return True
-    return False
-
-def visible_from_north(row, col, tree_data):
-    this_tree_size = int(tree_data[row][col])
-    for i in range(0, row):
-        if int(tree_data[i][col]) >= this_tree_size:
-            return False
-    return True
-
-def visible_from_south(row, col, tree_data):
-    this_tree_size = int(tree_data[row][col])
-    for i in range(row+1, len(tree_data)):
-        if int(tree_data[i][col]) >= this_tree_size:
-            return False
-    return True
-
-def visible_from_west(row, col, tree_data):
-    this_tree_size = int(tree_data[row][col])
-    for i in range(0, col):
-        if int(tree_data[row][i]) >= this_tree_size:
-            return False
-    return True
-
-def visible_from_east(row, col, tree_data):
-    this_tree_size = int(tree_data[row][col])
-    for i in range(col+1, len(tree_data[0])):
-        if int(tree_data[row][i]) >= this_tree_size:
-            print(f"{row},{col}: found bigger tree to east at {row},{i}")
-            return False
-    return True
+tree_on_edge = lambda: (row == 0 or row == len(tree_data)-1) or (col == 0 or col == len(tree_data[0])-1)
+visible_from_north = lambda row, col: not any(int(tree_data[i][col]) >= this_tree_size for i in range(0, row))
+visible_from_south = lambda row, col: not any(int(tree_data[i][col]) >= this_tree_size for i in range(row+1, len(tree_data)))
+visible_from_west = lambda row, col: not any(int(tree_data[row][i]) >= this_tree_size for i in range(0, col))
+visible_from_east = lambda row, col: not any(int(tree_data[row][i]) >= this_tree_size for i in range(col+1, len(tree_data[0])))
+is_visible = lambda row, col: visible_from_north(row, col) or visible_from_south(row, col) or visible_from_west(row, col) or visible_from_east(row, col)
+tree_data = [line.strip() for line in open_file("input.txt")]
 
 counter = 0
-for row_index, row in enumerate(tree_data):
-    for col_index, col in enumerate(row):
-        if tree_on_edge(row_index, col_index, tree_data):
-            counter += 1
-        elif visible_from_north(row_index, col_index, tree_data) or visible_from_south(row_index, col_index, tree_data) or visible_from_west(row_index, col_index, tree_data) or visible_from_east(row_index, col_index, tree_data):
-                counter += 1
+for r, row in enumerate(tree_data):
+    for c, col in enumerate(row):
+        this_tree_size = int(col)
+        counter += 1 if tree_on_edge() or is_visible(r, c) else 0
 
-print(counter)
+print(counter) # 1690
--- 2022/day08/day08b.py
+++ 2022/day08/day08b.py
@@ -1,32 +1,21 @@
 import sys, os
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'common'))
 from common import open_file
-
-def get_west_view(row, col, tree_data):
-    trees = []
-    for i in range(0, col+1):
-        trees.append(int(tree_data[row][i]))
-    trees.reverse()
-    return trees
-
-def get_east_view(row, col, tree_data):
-    trees = []
-    for i in range(col, len(tree_data[0])):
-        trees.append(int(tree_data[row][i]))
-    return trees
-
-def get_north_view(row, col, tree_data):
-    trees = []
-    for i in range(0, row+1):
-        trees.append(int(tree_data[i][col]))
-    trees.reverse()
-    return trees
-
-def get_south_view(row, col, tree_data):
-    trees = []
-    for i in range(row, len(tree_data)):
-        trees.append(int(tree_data[i][col]))
-    return trees
+  
+def get_view(r, c, direction):
+    if direction == "N":
+        tree_column = [row[c] for row in tree_data][0:r+1]
+        tree_column.reverse()
+        return tree_column
+    elif direction == "S":
+        tree_column = [row[c] for row in tree_data]
+        return tree_column[r:]
+    elif direction == "W":
+        tree_row = tree_data[r][:c+1]
+        tree_row.reverse()
+        return tree_row
+    elif direction == "E":    
+        return tree_data[r][c:]
 
 def get_score(list):
     if len(list) == 1:
@@ -37,19 +26,15 @@ def get_score(list):
             break
     return i+1
 
-file_content = open_file("input.txt")
-
-tree_data = []
-for line in file_content:
-    tree_data.append(list(line.strip()))
+tree_data = [list(line.strip()) for line in open_file("input.txt")]
 
 best_scenic_score = 0
 for row_index, row in enumerate(tree_data):
     for col_index, col in enumerate(row):
-        scenic_score = get_score(get_west_view(row_index, col_index, tree_data)) * \
-            get_score(get_east_view(row_index, col_index, tree_data)) * \
-            get_score(get_south_view(row_index, col_index, tree_data)) * \
-            get_score(get_north_view(row_index, col_index, tree_data))
+        scenic_score = get_score(get_view(row_index, col_index, "W")) * \
+            get_score(get_view(row_index, col_index, "E")) * \
+            get_score(get_view(row_index, col_index, "S")) * \
+            get_score(get_view(row_index, col_index, "N"))
         best_scenic_score = max (scenic_score, best_scenic_score)
 
 print(best_scenic_score) # 535680
