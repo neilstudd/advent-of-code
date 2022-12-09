179553f852bff6b00c199a1384d7612e3871297b
Neil Studd <github@neilstudd.com>
2022-12-09 07:52:42 +0000

    AoC 2022 day 9 (against the clock)

--- 2022/day09/day09a.py
+++ 2022/day09/day09a.py
@@ -2,8 +2,54 @@ import sys, os
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'common'))
 from common import open_file
 
-file_content = open_file("input.txt")
+print_grid = lambda: (print("".join(row)) for row in grid)
 
-for line in file_content:
-    # do stuff
-    print(line.strip())
+def mark_visited(x, y):
+    grid[y][x] = "#"
+
+def determine_tail_movement():
+    if cur_h["x"] == cur_t["x"] and cur_h["y"] == cur_t["y"]:
+        return # H covers T
+    elif abs(cur_h["y"] - cur_t["y"]) <= 1 and abs(cur_h["x"] - cur_t["x"]) <= 1:
+        return # already touching
+    elif cur_h["x"] == cur_t["x"]:
+        if cur_h["y"] > cur_t["y"]:
+            cur_t["y"] += 1
+        else:
+            cur_t["y"] -= 1
+    elif cur_h["y"] == cur_t["y"]:
+        if cur_h["x"] > cur_t["x"]:
+            cur_t["x"] += 1
+        else:
+            cur_t["x"] -= 1
+    else: # Diagonal?
+        if cur_h["x"] > cur_t["x"]:
+            if cur_h["y"] > cur_t["y"]:
+                cur_t["x"] += 1
+                cur_t["y"] += 1
+            else:
+                cur_t["x"] += 1
+                cur_t["y"] -= 1
+        else:
+            if cur_h["y"] > cur_t["y"]:
+                cur_t["x"] -= 1
+                cur_t["y"] += 1
+            else:
+                cur_t["x"] -= 1
+                cur_t["y"] -= 1
+
+# Build a grid with all squares unvisited
+grid = [["." for x in range(1000)] for y in range(1000)]
+
+cur_h = {"x": 0, "y": 0}
+cur_t = {"x": 0, "y": 0}
+
+for line in open_file("input.txt"):
+    dir, steps = line.strip().split(" ")
+    for i in range(int(steps)):
+        cur_h["x"] += 1 if dir == "R" else -1 if dir == "L" else 0
+        cur_h["y"] += 1 if dir == "D" else -1 if dir == "U" else 0
+        determine_tail_movement()
+        mark_visited(cur_t["x"], cur_t["y"])
+
+print(sum(row.count("#") for row in grid)) # 6044
--- 2022/day09/day09b.py
+++ 2022/day09/day09b.py
@@ -2,8 +2,60 @@ import sys, os
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'common'))
 from common import open_file
 
-file_content = open_file("input.txt")
+print_tail_grid = lambda: (print("".join(row)) for row in tail_grid)
 
-for line in file_content:
-    # do stuff
-    print(line.strip())
+def mark_tail_location():
+    x = rope[-1]["x"]
+    y = rope[-1]["y"]
+    tail_grid[y][x] = "#"
+
+def move_whole_rope():
+    for i in range(1, len(rope)):
+        move_this_link(rope[i-1], rope[i])
+    mark_tail_location()
+
+def move_this_link(a, b):
+    if a["x"] == b["x"] and a["y"] == b["y"]:
+        return # a covers b
+    elif abs(a["y"] - b["y"]) <= 1 and abs(a["x"] - b["x"]) <= 1:
+        return # already touching
+    elif a["x"] == b["x"]:
+        if a["y"] > b["y"]:
+            b["y"] += 1
+        else:
+            b["y"] -= 1
+    elif a["y"] == b["y"]:
+        if a["x"] > b["x"]:
+            b["x"] += 1
+        else:
+            b["x"] -= 1
+    else: # Diagonal?
+        if a["x"] > b["x"]:
+            if a["y"] > b["y"]:
+                b["x"] += 1
+                b["y"] += 1
+            else:
+                b["x"] += 1
+                b["y"] -= 1
+        else:
+            if a["y"] > b["y"]:
+                b["x"] -= 1
+                b["y"] += 1
+            else:
+                b["x"] -= 1
+                b["y"] -= 1 
+
+# Build a grid with all squares unvisited
+tail_grid = [["." for x in range(1000)] for y in range(1000)]
+
+# Hardcode the rope
+rope = [{"x": 0, "y": 0},{"x": 0, "y": 0},{"x": 0, "y": 0},{"x": 0, "y": 0},{"x": 0, "y": 0},{"x": 0, "y": 0},{"x": 0, "y": 0},{"x": 0, "y": 0},{"x": 0, "y": 0},{"x": 0, "y": 0}]
+
+for line in open_file("input.txt"):
+    dir, steps = line.strip().split(" ")
+    for i in range(int(steps)):
+        rope[0]["x"] += 1 if dir == "R" else -1 if dir == "L" else 0
+        rope[0]["y"] -= 1 if dir == "U" else -1 if dir == "D" else 0
+        move_whole_rope()
+
+print(sum(row.count("#") for row in tail_grid))   # 2384 
