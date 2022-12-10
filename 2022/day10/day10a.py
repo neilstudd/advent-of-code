6fd4d4c9bcda75a89d1f5631d0ce67570cca2357
Neil Studd <github@neilstudd.com>
2022-12-10 07:43:28 +0000

    AoC 2022 day 10

--- 2022/day10/day10a.py
+++ 2022/day10/day10a.py
@@ -2,8 +2,25 @@ import sys, os
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'common'))
 from common import open_file
 
-file_content = open_file("input.txt")
+def update_key_cycles():
+    global last_cycle_summed, summed_strengths
+    if cycle in key_cycles:
+        if last_cycle_summed != cycle:
+            summed_strengths += x * cycle
+            last_cycle_summed = cycle 
 
-for line in file_content:
-    # do stuff
-    print(line.strip())
+is_addx = lambda line: line.startswith("addx")
+key_cycles = [20, 60, 100, 140, 180, 220]
+cycle = 0
+x = 1
+last_cycle_summed = 0
+summed_strengths = 0
+
+for line in open_file("input.txt"):
+    cycle += 1
+    update_key_cycles()
+    cycle += 1 if is_addx(line) else 0
+    update_key_cycles()
+    x += int(line.split(" ")[1]) if is_addx(line) else 0
+
+print(summed_strengths) # 16880
--- 2022/day10/day10b.py
+++ 2022/day10/day10b.py
@@ -2,8 +2,30 @@ import sys, os
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'common'))
 from common import open_file
 
-file_content = open_file("input.txt")
+def render_crt():
+    for i in range(len(crt)//40):
+        print(crt[i*40:(i+1)*40-1])
 
-for line in file_content:
-    # do stuff
-    print(line.strip())
+def update_crt():
+    global crt
+    pixel_range = range(x-1, x+2)
+    if cycle-1 in pixel_range or cycle-41 in pixel_range or \
+        cycle-81 in pixel_range or cycle-121 in pixel_range or \
+        cycle-161 in pixel_range or cycle-201 in pixel_range or \
+        cycle-241 in pixel_range:
+            crt = crt[:cycle-1] + "#" + crt[cycle:]
+    
+is_addx = lambda line: line.startswith("addx")
+
+crt = "." * 240
+cycle = 0
+x = 1
+
+for line in open_file("input.txt"):
+    cycle += 1
+    update_crt()
+    cycle += 1 if is_addx(line) else 0
+    update_crt()
+    x += int(line.split(" ")[1]) if is_addx(line) else 0
+
+print(render_crt()) # RKAZAJBR
