c3952e614d12dca1d7af2d37e10d4b6528e8c719
Neil Studd <github@neilstudd.com>
2022-12-12 14:20:21 +0000

    AoC day 12: failed

--- 2022/day12/day12a.py
+++ 2022/day12/day12a.py
@@ -2,8 +2,44 @@ import sys, os
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'common'))
 from common import open_file
 
-file_content = open_file("input.txt")
+def determine_valid_directions(x, y, char):
+    if y < len(terrain) - 1:
+        if ord(terrain[y + 1][x]["char"]) - ord(char) <= 1:
+            terrain[y][x]["directions"] += "S"
+    if y > 0:
+        if  ord(terrain[y - 1][x]["char"]) - ord(char) <= 1:
+            terrain[y][x]["directions"] += "N"
+    if x < len(terrain[y]) - 1:
+        if ord(terrain[y][x + 1]["char"]) - ord(char) <= 1:
+            terrain[y][x]["directions"] += "E"
+    if x > 0:
+        if ord(terrain[y][x - 1]["char"]) - ord(char) <= 1:
+            terrain[y][x]["directions"] += "W"
 
-for line in file_content:
-    # do stuff
-    print(line.strip())
+terrain = []
+for line in open_file("input.txt"):
+    this_line = []
+    for char in line.strip():
+        this_line.append({"char": char, "directions": ""})
+    terrain.append(this_line)
+
+# Locate the start and end points
+for y, line in enumerate(terrain):
+    for x, char in enumerate(line):
+        if char["char"] == "S":
+            start_point = (x,y)
+            terrain[y][x]["char"] = "a"
+        if char["char"] == "E":
+            end_point = (x,y)
+            terrain[y][x]["char"] = "z"
+
+# Iterate over terrain and determine valid directions for each char
+for y, line in enumerate(terrain):
+    for x, char in enumerate(line):
+        determine_valid_directions(x, y, char["char"])
+
+print(terrain)
+
+# FAILED_CHALLENGE
+# We have a start_point, an end_point, and each char has a list of valid directions
+# Need to algorithmically determine the shortest path from start to end
