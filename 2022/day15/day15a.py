79afab9165bf9142654578ea4cbd566c07218871
Neil Studd <github@neilstudd.com>
2022-12-15 07:58:57 +0000

    Add last will and testament to day 15

--- 2022/day15/day15a.py
+++ 2022/day15/day15a.py
@@ -15,7 +15,10 @@ def print_grid():
 # Even numpy explodes
 # grid = np.full((4000000, 4000000), ".")
 
-grid = [["." for x in range(100)] for y in range(100)]
+# lil_matrix seems to be the way to go:
+# grid = lil_matrix((4000000, 4000000), dtype=np.int8)
+# ...though it doesn't like negative indices, so ended up having to offset everything
+# eg initialise as 8000000x8000000, then set FAKE_OFFSET = 4000000 and apply to everything
 
 for line in open_file("input.txt"):
     sensor_x = int(line.split(" ")[2].split(",")[0].split("=")[1])
@@ -24,6 +27,7 @@ for line in open_file("input.txt"):
     beacon_y = int(line.split(" ")[9].split(":")[0].split("=")[1])
     distance = abs(sensor_x - beacon_x) + abs(sensor_y - beacon_y)
 
+    # Even when using lil_matrix, this is too big to generate...
     for x in range(sensor_x - distance, sensor_x + distance + 1):
         for y in range(sensor_y - distance, sensor_y + distance + 1):
             if abs(sensor_x - x) + abs(sensor_y - y) <= distance:
