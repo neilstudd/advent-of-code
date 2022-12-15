29d3fc319538f7ef04fd85954a7d359e4fa4bb15
Neil Studd <github@neilstudd.com>
2022-12-15 07:59:33 +0000

    Guess I won't be needing THESE any more

--- 2022/day20/day20a.py
+++
@@ -1,9 +0,0 @@
-import sys, os
-sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'common'))
-from common import open_file
-
-file_content = open_file("input.txt")
-
-for line in file_content:
-    # do stuff
-    print(line.strip())
