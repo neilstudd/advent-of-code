55584fb2d6fe5217a74de0a54c56f25daf655891
Neil Studd <github@neilstudd.com>
2022-12-18 15:46:00 +0000

    Day 18: part 2 still not working, but feels like progress

--- 2022/day18/day18.py
+++ 2022/day18/day18.py
@@ -5,15 +5,35 @@ from common import open_file
 def create_grid(x, y, z):
     return [[[0 for k in range(z)] for j in range(y)] for i in range(x)]
 
+def create_exposure_grid(x, y, z):
+    return [[[None for k in range(z)] for j in range(y)] for i in range(x)]
+
 def is_exposed(x, y, z):
-    # TODO: Implement this
-    return True
+    return exposed[x][y][z]
 
-cubes = create_grid(20, 20, 20)
+cubes = create_grid(30, 30, 30)
+exposed = create_exposure_grid(30, 30, 30)
 
-for line in  open_file("input.txt"):
+for line in open_file("input.txt"):
     x, y, z = line.strip().split(",")
     cubes[int(x)][int(y)][int(z)] = 1
+
+# Traverse through all nodes in cubes, and calculate if that node has a route to the edge
+# (i.e. it connects to a line of 0s)
+for x in range(len(cubes)):
+    for y in range(len(cubes[x])):
+        for z in range(len(cubes[x][y])):
+            if cubes[x][y][z] == 0:
+                if x == 0 or cubes[x-1][y][z] == 0: exposed[x][y][z] = True
+                elif y == 0 or cubes[x][y-1][z] == 0: exposed[x][y][z] = True
+                elif z == 0 or cubes[x][y][z-1] == 0: exposed[x][y][z] = True
+                elif x == len(cubes)-1 or cubes[x+1][y][z] == 0: exposed[x][y][z] = True
+                elif y == len(cubes[x])-1 or cubes[x][y+1][z] == 0: exposed[x][y][z] = True
+                elif z == len(cubes[x][y])-1 or cubes[x][y][z+1] == 0: exposed[x][y][z] = True
+                else:
+                    exposed[x][y][z] = False
+            else:
+                exposed[x][y][z] = False
 
 # Count outside faces
 num_faces = 0
@@ -35,24 +55,11 @@ for x in range(len(cubes)):
     for y in range(len(cubes[x])):
         for z in range(len(cubes[x][y])):
             if cubes[x][y][z] != 0:
-                if x == 0: num_exposed_faces += 1
-                elif cubes[x-1][y][z] == 0:
-                    if is_exposed(x-1, y, z): num_exposed_faces += 1
-                if x == len(cubes)-1: num_exposed_faces += 1
-                elif cubes[x+1][y][z] == 0:
-                    if is_exposed(x+1, y, z): num_exposed_faces += 1
-                if y == 0: num_exposed_faces += 1
-                elif cubes[x][y-1][z] == 0:
-                    if is_exposed(x, y-1, z): num_exposed_faces += 1 
-                if y == len(cubes[x])-1: num_exposed_faces += 1
-                elif cubes[x][y+1][z] == 0:
-                    if is_exposed(x, y+1, z): num_exposed_faces += 1
-                if z == 0: num_exposed_faces += 1
-                elif cubes[x][y][z-1] == 0:
-                    if is_exposed(x, y, z-1): num_exposed_faces += 1
-                # Check if the back face is not touching another cube
-                if z == len(cubes[x][y])-1: num_exposed_faces += 1
-                elif cubes[x][y][z+1] == 0:
-                    if is_exposed(x, y, z+1): num_exposed_faces += 1
+                if is_exposed(x-1, y, z): num_exposed_faces += 1
+                if is_exposed(x+1, y, z): num_exposed_faces += 1
+                if is_exposed(x, y-1, z): num_exposed_faces += 1
+                if is_exposed(x, y+1, z): num_exposed_faces += 1
+                if is_exposed(x, y, z-1): num_exposed_faces += 1
+                if is_exposed(x, y, z+1): num_exposed_faces += 1
 
-print(f"Part 2: {num_exposed_faces}")
+print(f"Part 2: {num_exposed_faces}") # 1792 too low, 3023 too high
