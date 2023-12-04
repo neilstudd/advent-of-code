cd81241a448b4cb47763816eb58aea330beec307
Neil Studd <github@neilstudd.com>
2023-12-04 08:58:17 +0000

    Day 4 huge success

--- 2023/day04/day04.py
+++ 2023/day04/day04.py
@@ -4,28 +4,58 @@ from common import open_file, print_and_verify_answer
 
 def run_part_one(mode, expected = None):
     data_file = open_file( mode + ".txt")
-
-    # -------------------------------
-    # Part One code goes here
-
-    answer = None # <-- Change this to answer
-    # -------------------------------
-    print_and_verify_answer(mode, "one", answer, expected)
+    points = 0
+    for card in data_file:
+        parts = card.split(':')
+        winning_numbers, your_numbers = calculate_numbers(parts[1])
+        this_card_points = 0
+        for num in winning_numbers:
+            if num in your_numbers:
+                if this_card_points == 0:
+                    this_card_points = 1
+                else:
+                    this_card_points *= 2
+        points += this_card_points
+    print_and_verify_answer(mode, "one", points, expected)
 
 def run_part_two(mode, expected = None):
-
     data_file = open_file( mode + ".txt")
+    card_list = []
+    card_stats = []
+    
+    for line in data_file:
+        card_list.append(line.strip())
+    
+    for card in card_list:
+        parts = card.split(':')
+        card_number = int(parts[0].split()[-1])
+        winning_numbers, your_numbers = calculate_numbers(parts[1])
+        matching_numbers = 0
+        for num in winning_numbers:
+            if num in your_numbers:
+                matching_numbers += 1
+        card_stats.append({"number": card_number, "matches": matching_numbers, "count": 1})
 
-    # -------------------------------
-    # Part Two code goes here
+    for card in card_stats:
+        for _ in range(card["count"]):
+            for i in range(card["matches"]):
+                card_stats[card["number"]+i]["count"] += 1
 
-    answer = None # <-- Change this to answer
-    # -------------------------------
-    print_and_verify_answer(mode, "two", answer, expected)
+    total = sum(card["count"] for card in card_stats)
+    print_and_verify_answer(mode, "two", total, expected)
+
+def calculate_numbers(data):
+    numbers = data.strip().split('|')
+    winning_numbers = [int(num) for num in numbers[0].split()]
+    your_numbers = [int(num) for num in numbers[1].split()]
+    return winning_numbers, your_numbers
+
+def is_copy(card):
+    return card.startswith("Copy")
 
 # ADD EXPECTED OUTPUTS TO TESTS HERE 👇
-run_part_one("test", 0)
-run_part_one("prod")
-run_part_two("test", 0)
-run_part_two("prod")
+run_part_one("test", 13)
+run_part_one("prod", 20829)
+run_part_two("test", 30)
+run_part_two("prod", 12648035)
 # Now run it and watch the magic happen 🪄
