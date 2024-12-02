c159d9a3f0c6294ca8436e570ad9eabf64163634
Neil Studd <neil.studd@makers.tech>
2024-12-02 11:59:17 +0000

    day2 refactor

--- 2024/day02/day02.py
+++ 2024/day02/day02.py
@@ -4,14 +4,15 @@ from common import open_file, print_and_verify_answer
 
 def calculate_level_difference(levels):
     differences = []
-    last_level = None
+    previous_level = None
     for level in levels:
-        if not last_level:
-            last_level = int(level)
-        else:
-            differences.append(int(level) - last_level)
-            last_level = int(level)
+        if previous_level:
+            differences.append(int(level) - previous_level)
+        previous_level = int(level)
     return differences
+
+def are_differences_valid(differences):
+    return all((x > 0 and x <= 3) for x in differences) or all((x < 0 and x >= -3) for x in differences)
 
 def run_part_one(mode, expected = None):
     data_file = open_file( mode + ".txt")
@@ -19,19 +20,19 @@ def run_part_one(mode, expected = None):
     for line in data_file:
         levels = line.split()
         differences = calculate_level_difference(levels)
-        if all((x > 0 and x <= 3) for x in differences) or all((x < 0 and x >= -3) for x in differences):
-            safe_reports += 1        
+        if are_differences_valid(differences):
+            safe_reports += 1
     print_and_verify_answer(mode, "one", safe_reports, expected)
 
 def run_part_two(mode, expected = None):
     data_file = open_file( mode + ".txt")
     safe_reports = 0
     for line in data_file:        
-        levels = list(map(int, line.split()))
+        levels = line.split()
         all_combos_of_levels_with_one_removed = [levels[:i] + levels[i+1:] for i in range(len(levels))]
         for combo in all_combos_of_levels_with_one_removed:
             differences = calculate_level_difference(combo)
-            if all((x > 0 and x <= 3) for x in differences) or all((x < 0 and x >= -3) for x in differences):
+            if are_differences_valid(differences):
                 safe_reports += 1
                 break
     print_and_verify_answer(mode, "two", safe_reports, expected)
