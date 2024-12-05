915bbb74443bc7291c363523a6507a9f24ad7c4d
Neil Studd <neil.studd@makers.tech>
2024-12-05 09:28:01 +0000

    Big refactor although part 2 is still slow

--- 2024/day05/day05.py
+++ 2024/day05/day05.py
@@ -2,7 +2,11 @@ import sys, os
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'common'))
 from common import open_file, print_and_verify_answer
 
+page_ordering_rules = []
+updates = []
+
 def process_file(data_file):
+    global page_ordering_rules, updates
     page_ordering_rules = []
     updates = []
     for line in data_file:
@@ -14,80 +18,76 @@ def process_file(data_file):
             updates.append(this_update)
     return page_ordering_rules, updates
 
-def pages_which_must_come_after(page, page_ordering_rules):
+def is_update_valid(update):
+    global page_ordering_rules
+    for page_index, page in enumerate(update):
+        pages_which_come_before = get_all_updates_before_index(page_index, update)
+        for prev_page in pages_which_come_before:
+            if prev_page in pages_which_must_come_after(page):
+                return False
+    return True
+
+def pages_which_must_come_after(page):
+    global page_ordering_rules
     pages = []
     for rule in page_ordering_rules:
         if rule[0] == page:
             pages.append(rule[1])
     return pages
 
-def pages_which_must_come_before(page, page_ordering_rules):
+def pages_which_must_come_before(page):
+    global page_ordering_rules
     pages = []
     for rule in page_ordering_rules:
         if rule[1] == page:
             pages.append(rule[0])
     return pages
 
-def get_all_pages_after_index(page_index, updates):
+def get_all_pages_after_index(page_index):
+    global updates
     if page_index == len(updates) - 1:
         return []
     return updates[page_index+1:]
 
-def get_all_pages_before_index(page_index, update):
+def get_all_updates_before_index(page_index, update):
     if page_index == 0:
         return []
     return update[:page_index]
 
-def sort_updates_by_page_ordering_rules(updates, page_ordering_rules):
-    valid_updates = []
-    invalid_updates = []
-    for update in updates:
-        update_valid = True
-        for page_index, page in enumerate(update):
-            pages_which_come_before = get_all_pages_before_index(page_index, update)
-            for prev_page in pages_which_come_before:
-                if prev_page in pages_which_must_come_after(page, page_ordering_rules):
-                    update_valid = False      
-        if update_valid:
-            valid_updates.append(update)
-        else:
-            invalid_updates.append(update)
+def partition_updates_by_validity():
+    global updates, page_ordering_rules
+    valid_updates = [update for update in updates if is_update_valid(update)]
+    invalid_updates = [update for update in updates if not is_update_valid(update)]
     return valid_updates, invalid_updates
             
-def are_there_any_invalid_updates_left(updates, page_ordering_rules):
-    new_valid_updates, new_invalid_updates = sort_updates_by_page_ordering_rules(updates, page_ordering_rules)
-    return len(new_invalid_updates) > 0
+def are_there_any_invalid_updates_left():
+    global updates
+    return any(not is_update_valid(update) for update in updates)
+
+def sum_the_middles(updates):
+    return sum(int(update[len(update) // 2]) for update in updates)
 
 def run_part_one(mode, expected = None):
+    global page_ordering_rules, updates
     data_file = open_file( mode + ".txt")
     page_ordering_rules, updates = process_file(data_file)
-    valid_updates, invalid_updates = sort_updates_by_page_ordering_rules(updates, page_ordering_rules)
-    sum_of_middle_numbers = 0
-    for valid_update in valid_updates:
-        middle_index = len(valid_update) // 2
-        sum_of_middle_numbers += int(valid_update[middle_index])
+    valid_updates, _ = partition_updates_by_validity()
+    sum_of_middle_numbers = sum_the_middles(valid_updates)
     print_and_verify_answer(mode, "one", sum_of_middle_numbers, expected)
 
 def run_part_two(mode, expected = None):
+    global page_ordering_rules, updates
     data_file = open_file( mode + ".txt")
     page_ordering_rules, updates = process_file(data_file)
-    valid_updates, invalid_updates = sort_updates_by_page_ordering_rules(updates, page_ordering_rules)
-    while are_there_any_invalid_updates_left(invalid_updates, page_ordering_rules):
+    _, invalid_updates = partition_updates_by_validity()
+    while are_there_any_invalid_updates_left():
         for invalid_update in invalid_updates:
             for page in invalid_update:
-                # find the index furthest to the right that it must come after
-                must_come_after_these = pages_which_must_come_before(page, page_ordering_rules)
-                furthest_to_the_right = -1
-                for must_come_after in must_come_after_these:
-                    if must_come_after in invalid_update:
-                        furthest_to_the_right = max(furthest_to_the_right, invalid_update.index(must_come_after))
-                # move page to the right of furthest_to_the_right
+                must_come_after_these = pages_which_must_come_before(page)
+                furthest_to_the_right = max((invalid_update.index(must_come_after) for must_come_after in must_come_after_these if must_come_after in invalid_update), default=-1)
                 invalid_update.remove(page)
                 invalid_update.insert(furthest_to_the_right + 1, page)
-    sum_of_middle_numbers = 0
-    for invalid_update in invalid_updates:
-        middle_index = len(invalid_update) // 2
-        sum_of_middle_numbers += int(invalid_update[middle_index])
+    sum_of_middle_numbers = sum_the_middles(invalid_updates)
     print_and_verify_answer(mode, "two", sum_of_middle_numbers, expected)
 
 # ADD EXPECTED OUTPUTS TO TESTS HERE 👇
