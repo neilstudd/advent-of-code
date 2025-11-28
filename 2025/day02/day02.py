cc3d23de49b4d89d34f337f67504183af3d7ee76
Neil Studd <neil.studd@makers.tech>
2025-11-28 11:25:31 +0000

    Empty files ready for 2025

---
+++ 2025/day02/day02.py
@@ -0,0 +1,16 @@
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'common'))
+from common import open_file, print_and_verify_answer
+
+def run_part_one(mode, expected = None):
+    pass
+
+def run_part_two(mode, expected = None):
+    pass
+
+# ADD EXPECTED OUTPUTS TO TESTS HERE 👇
+run_part_one("test", 0)
+run_part_one("prod", 0)
+run_part_two("test", 0)
+run_part_two("prod", 0)
+# Now run it and watch the magic happen 🪄
