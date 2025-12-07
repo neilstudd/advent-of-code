7202ab0311c80eddfc4e75ebf3f5124417620a19
Neil Studd <neil.studd@makers.tech>
2025-12-07 17:57:13 +0000

    AoC day 7 pt2 win

--- 2025/day07/day07.py
+++ 2025/day07/day07.py
@@ -6,35 +6,40 @@ def run_part_one(mode, expected = None):
     grid_data = open_file( mode + ".txt")
     grid = initialise_grid(grid_data)
     for line in grid:
-        # check if S in line
         if "S" in line:
             beam_indices = [line.index("S")]
         else:
             new_beam_indices = []
             for index in beam_indices:
                 if line[index] == ".":
-                    line[index] = "|"
                     new_beam_indices.append(index)
                 elif line[index] == "^":
                     line[index] = "X" # we hit this splitter
-                    if index - 1 >= 0:
-                        new_beam_indices.append(index - 1)
-                        line[index-1] = "|"
-                    if index + 1 < len(line):
-                        new_beam_indices.append(index + 1)
-                        line[index+1] = "|"
+                    new_beam_indices.append(index - 1)
+                    new_beam_indices.append(index + 1)
             beam_indices = new_beam_indices
-
-    # count the number of X's (the number of splitters we hit)
     beam_hit_count = sum(line.count("X") for line in grid)
     print_and_verify_answer(mode, "One", beam_hit_count, expected)
 
 def run_part_two(mode, expected = None):
-    pass
+    grid_data = open_file( mode + ".txt")
+    grid = initialise_grid(grid_data)
+    beam_indices = {}
+    for line in grid:
+        if "S" in line:
+            beam_indices = {line.index("S"): 1}
+        else:
+            for index, count in list(beam_indices.items()):
+                if line[index] == "^":
+                    beam_indices[index] = 0 # no longer a beam in this column
+                    beam_indices[index - 1] = beam_indices.get(index - 1, 0) + count
+                    beam_indices[index + 1] = beam_indices.get(index + 1, 0) + count
+    total_ways = sum(beam_indices.values())
+    print_and_verify_answer(mode, "Two", total_ways, expected)
 
 # ADD EXPECTED OUTPUTS TO TESTS HERE 👇
 run_part_one("test", 21)
 run_part_one("prod", 1585)
-run_part_two("test", 0)
-run_part_two("prod", 0)
+run_part_two("test", 40)
+run_part_two("prod", 16716444407407) # FIRST SUBMIT: "435 too low" (lol)
 # Now run it and watch the magic happen 🪄
