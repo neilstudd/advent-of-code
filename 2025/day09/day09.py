73fb587587f3a31ba5d7be2ab977dfa4b462accf
Neil Studd <neil.studd@makers.tech>
2025-12-09 08:09:26 +0000

    AoC day 9 pt1.1: refactor to make pt2 easier?

--- 2025/day09/day09.py
+++ 2025/day09/day09.py
@@ -2,25 +2,31 @@ import sys, os
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'common'))
 from common import open_file, print_and_verify_answer
 
-def run_part_one(mode, expected = None):
+def calculate_tiles(mode):
     tile_coords = set()
     tiles = open_file( mode + ".txt")
     for tile in tiles:
         x, y = map(int, tile.split(","))
         tile_coords.add((x, y))
-    largest_rectangle = 0
+    return tile_coords
+
+def calculate_all_rectangles(tile_coords):
+    rectangles = []
     for tile in tile_coords:
         for other_tile in tile_coords:
             if tile != other_tile:
                 x1, y1 = tile
                 x2, y2 = other_tile
                 area = (abs(x2 - x1)+1) * (abs(y2 - y1)+1)
-                if area > largest_rectangle:
-                    largest_rectangle = area
+                rectangles.append(((tile, other_tile), area))
+    rectangles.sort(key=lambda x: x[1], reverse=True) # descending order of area
+    return rectangles
 
-    print_and_verify_answer(mode, "one", largest_rectangle, expected)
+def run_part_one(mode, expected = None):
+    tile_coords = calculate_tiles(mode)
+    rectangles = calculate_all_rectangles(tile_coords)
+    print_and_verify_answer(mode, "one", rectangles[0][1], expected)
     
-
 def run_part_two(mode, expected = None):
     pass
 
